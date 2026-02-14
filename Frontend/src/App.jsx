5e7ea7a1ce22f8a4f19f801025827b56c8f06a4e
Fahad <fahadanwar5925@gmail.com>
2026-02-14 12:25:22 +0530

    Setup React Frontend with Login and Signup pages.

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+{/*import './App.css'*/}
+
+{/*import SignUp from "./Pages/SignUp";*/}
+import Login from "./Pages/Login";
+
+function App() {
+  return (
+    <div> 
+      {/*<SignupPage />*/}
+      <Login />
+    </div>
+    
+  );
+}
+
+export default App
