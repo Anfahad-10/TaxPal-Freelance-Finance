5dde748a921611d2faa8ae675cc3a3d0f2ddac64
Fahad <fahadanwar5925@gmail.com>
2026-02-16 17:21:04 +0530

    added Login and Register backend part

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,15 @@
-const express = require("Express")
-
-
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const authRouter = require("./routes/auth.routes")
 
 
 const app = express()
 
 
+app.use(express.json())
+app.use(cookieParser())
 
-module.exports = app
+
+app.use("/api/auth", authRouter)
+
+module.exports = app 
